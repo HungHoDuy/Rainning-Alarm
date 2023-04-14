a46186cabb50175cd0216e342b04fb374e425613
HungHoDuy <122082132+HungHoDuy@users.noreply.github.com>
2023-04-15 00:38:51 +0700

    Update weather forecast.py

--- weather forecast.py
+++ weather forecast.py
@@ -27,7 +27,14 @@ class RainProbabilityApp:
 
         self.check_button = tk.Button(self.input_frame, text="Check", bg="#7289DA", fg="#FFFFFF",
                                       command=self.get_weather_btn_click)
+
         self.check_button.pack(side=tk.LEFT, padx=10)
+        self.status_label = tk.Label(self.input_frame, text="", fg="blue")
+        self.status_label.pack(side=tk.LEFT, padx=10)
+
+        self.forecast_label = tk.Label(self.input_frame, text="")
+        self.forecast_label.pack(side=tk.LEFT, padx=10)
+
 
     def get_rain_probability(self, city):
         api_key = "e71f41462402ed8af85f57e58283926b"
@@ -52,24 +59,31 @@ class RainProbabilityApp:
             next_hour_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(next_hour_forecast['dt']))
             next_hour_precipitation = next_hour_forecast['pop'] * 100
 
-            print(f"Forecast data for {city}:")
-            print(f"Time: {next_hour_time}")
-            print(f"Rain probability: {next_hour_precipitation}")
+            self.forecast_label.config(
+                text=f"Forecast data for {city}:\nTime: {next_hour_time}\nRain probability: {next_hour_precipitation}%")
+            self.forecast_label.pack(side=tk.LEFT, padx=10)
 
-            return next_hour_precipitation >= 50
+            return next_hour_precipitation >= 50, {"time": next_hour_time, "precipitation": next_hour_precipitation}
         except (requests.exceptions.RequestException, ValueError):
             print(f"Unable to get forecast data for {city}")
             return False
 
     def check_rain_probability(self, city):
+        self.city_entry.config(state=tk.DISABLED)
+
         def check_loop():
             while True:
-                rain_probability = self.get_rain_probability(city)
+                rain_probability, forecast_data = self.get_rain_probability(city)
                 if rain_probability:
+                    self.status_label.config(
+                        text=f"There's a greater than or equal to 50% chance of rain in {city} in the next hour.")
                     messagebox.showwarning("Rain Alert",
                                            f"There's a greater than or equal to 50% chance of rain in {city} in the next hour.")
                     break
                 else:
+                    self.status_label.config(text="Checking...")
+                    self.forecast_label.config(
+                        text=f"Forecast data for {city}:\nTime: {forecast_data['time']}\nRain probability: {forecast_data['precipitation']}")
                     time.sleep(600)
 
         t = threading.Thread(target=check_loop)
@@ -84,39 +98,3 @@ if __name__ == "__main__":
     root = tk.Tk()
     app = RainProbabilityApp(root)
     root.mainloop()
-
-#root = tk.Tk()
-root.geometry("750x450")
-root.title("Todo App")
-root.configure(background="#36393F")
-
-title_label = ttk.Label(root, text="Daily Tasks", foreground="#FFFFFF", background="#36393F", font=("Whitney", 30, "bold"))
-title_label.pack(padx=10, pady=(40, 20))
-
-scrollable_frame = ttk.Frame(root)
-scrollable_frame.pack(pady=10)
-
-canvas = tk.Canvas(scrollable_frame)
-canvas.pack(side="left", fill="both", expand=True)
-
-scrollbar = ttk.Scrollbar(scrollable_frame, orient="vertical", command=canvas.yview)
-scrollbar.pack(side="right", fill="y")
-
-canvas.configure(yscrollcommand=scrollbar.set)
-canvas.bind('<Configure>', lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
-
-scrollable_frame.interior = ttk.Frame(canvas)
-canvas.create_window((0, 0), window=scrollable_frame.interior, anchor="nw")
-
-entry = ttk.Entry(root, width=40, font=("Whitney", 12))
-entry.pack(padx=10, pady=10, fill="x")
-
-add_button = ttk.Button(root, text="Add", width=10, command=add_todo)
-add_button.pack(pady=10)
-
-# Set the style of the scrollbar
-style = ttk.Style()
-style.configure("Vertical.TScrollbar", background="#2C2F33")
-
-# Run the main loop
-root.mainloop()
